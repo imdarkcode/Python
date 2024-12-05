9fd34cbcd0f05fb58170c9a82bd44cad33870d52
Otavio <otavio.sudano@email.com>
2024-12-05 10:56:02 -0300

    Adicionando tipo-de-dados.py

---
+++ tipos-de-dados.py
@@ -0,0 +1,16 @@
+# Strings: são elemendos dentro de aspas
+print("Python")
+
+# Int: são números inteiros
+print(11)
+print(-28)
+print(0)
+
+# Float: são números com casas decimais separadas por ponto
+print(4.24)
+print(-74.1)
+print(0.0)
+
+# Bool: são as valores binários de resposta de verdadeiro (True) ou falso (False)
+print(True)
+print(False)
