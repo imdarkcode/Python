e6bc835de5fb50ab0574059fd552b644bcb9f06b
Otavio <otavio.sudano@email.com>
2024-12-05 10:55:23 -0300

    Adicionando comentarios.py

---
+++ comentarios.py
@@ -0,0 +1,11 @@
+# Permite escrever um comentário na linha
+
+"""
+Permite escrever um comentário em
+multiplas linhas.
+"""
+
+'''
+O comentário em multiplas linhas também
+funciona com aspas simples.
+'''
