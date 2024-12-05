e32a6711e2e0a218ecd5e3d957b95ad08649b78c
Otavio <otavio.sudano@email.com>
2024-12-05 10:55:37 -0300

    Adicionando print.py

---
+++ print.py
@@ -0,0 +1,12 @@
+# A função print permite mostrar um elemento na tela
+print(1234)
+
+# É possível separar os elemntos utilizando vígula
+print(12, 34)
+
+# Por padrão os elemento são separados por um espaço, é possível mudar isso com o atributo "sep"
+print(12, 34, sep="-")
+
+# Por padrão o print sempre vai terminar com uma quebra de linha, é possível mudar isso com o atributo "end"
+print(1234, end="-")
+print(5678)
