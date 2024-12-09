d7e267f02c3bb0f3eaac57298e638cee5a82827a
Otavio <otavio.sudano@email.com>
2024-12-09 10:40:59 -0300

    Adicionando operadores.py

---
+++ operadores.py
@@ -0,0 +1,34 @@
+"""
+Operadores aritméticos
+"""
+
+adicao = 10 + 10
+print("Adição:", adicao)
+
+subtracao = 84 - 23
+print("Subtração:", subtracao)
+
+multiplicacao = 5 * 2
+print("Multiplicação:", multiplicacao)
+
+divisao = 80 / 3 # Sempre vai retornar um valor float
+print("Divisão:", divisao)
+
+divisao_inteira = 80 // 3 # Sempre vai retornar um valor arredondado
+print("Divisão inteira:", divisao_inteira)
+
+potenciacao = 2 ** 10
+print("Potenciação:", potenciacao)
+
+modulo = 55 % 3 # Retorna o resto da divisão
+print("Modulo:", modulo)
+
+"""
+Operadores de concatenação e repetição
+"""
+
+texto = "A" + "B" + "C"
+print("Texto:", texto)
+
+texto_repetido = "A" * 3
+print("Texto repetido:", texto_repetido)
