8dc334edf350187ac4ac1579f4fd016a45211b40
Otavio <otavio.sudano@email.com>
2024-12-09 10:40:23 -0300

    Alterando tipos-de-dados.py

--- tipos-de-dados.py
+++ tipos-de-dados.py
@@ -1,16 +1,46 @@
-# Strings: são elemendos dentro de aspas
-print("Python")
+"""
+String são textos dentro de aspas duplas ou simples
+"""
 
-# Int: são números inteiros
+print("Texto")
+
+"""
+Int representa qualquer número positivo ou negativo
+"""
+
 print(11)
 print(-28)
 print(0)
 
-# Float: são números com casas decimais separadas por ponto
+"""
+Float representa qualquer número positivo ou negativo com ponto flutuante (decimal)
+"""
+
 print(4.24)
 print(-74.1)
 print(0.0)
 
-# Bool: são as valores binários de resposta de verdadeiro (True) ou falso (False)
-print(True)
-print(False)
+"""
+Boolean é o resultado de uma condição podendo ser verdadeiro (True) ou falso (False)
+"""
+
+print(0>10)
+print(1<12)
+
+"""
+É possível converter o tipo do dado utilizando funções específicas
+"""
+
+str(123) # Converter para string
+int("12") # Converter para inteiro
+float(12) # Converter para float
+bool("True") # Converter para boolean
+
+"""
+Utilizando a função type é possível ver o tipo do dado
+"""
+
+print(type("abc"))
+print(type(12))
+print(type(-2.45))
+print(type(0==7))
