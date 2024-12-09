0e0b30d7bac40e49a50c3b1b52d24bb91a018362
Otavio <otavio.sudano@email.com>
2024-12-09 10:41:16 -0300

    Adicionando variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,14 @@
+"""
+Variáveis são usadas para salvar algo na memória do computador,
+utilizando o sinal de = permite atribuir um valor a variável
+"""
+
+nome = "Henrique"
+sobrenome = "Castro"
+idade = 24
+email = "henrique@email.com"
+
+print("Nome:", nome, sobrenome)
+print("Idade:", idade)
+print("Email:", email)
+
